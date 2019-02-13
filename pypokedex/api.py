55e048d05ba3ec1a36c6594e0a62a75f6ace8a8d
Arnav Borborah <arnavborborah11@gmail.com>
2019-02-13 08:13:28 -0500

    Add pylint and black for linting and autoformatting

--- pypokedex/api.py
+++ pypokedex/api.py
@@ -5,35 +5,35 @@ import requests
 from pypokedex.exceptions import PyPokedexError, PyPokedexHTTPError
 from pypokedex.pokemon import Pokemon
 
+POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
 
 @lru_cache(maxsize=None)
 def get(**kwargs) -> Pokemon:
     if len(kwargs) != 1:
-        raise TypeError('pypokedex.get() expects expects only 1 argument!')
+        raise TypeError("pypokedex.get() expects expects only 1 argument!")
 
-    POKEAPI_BASE_URL = 'https://pokeapi.co/api/v2/pokemon'
-
-    if 'dex' in kwargs and isinstance(kwargs['dex'], int):
-        subpage = kwargs['dex']
-    elif 'name' in kwargs and isinstance(kwargs['name'], str):
-        subpage = kwargs['name'].lower()  # type: ignore
+    if "dex" in kwargs and isinstance(kwargs["dex"], int):
+        subpage = kwargs["dex"]
+    elif "name" in kwargs and isinstance(kwargs["name"], str):
+        subpage = kwargs["name"].lower()  # type: ignore
     else:
-        raise TypeError('Arguments were either of an incorrect type or value!')
+        raise TypeError("Arguments were either of an incorrect type or value!")
 
     try:
-        response = requests.get(f'{POKEAPI_BASE_URL}/{subpage}', timeout=3)
+        response = requests.get(f"{POKEAPI_BASE_URL}/{subpage}", timeout=3)
         response.raise_for_status()
 
     except requests.exceptions.HTTPError as error:
         if response.status_code == 404:
-            raise PyPokedexHTTPError('The requested pokemon was not '
-                                     'found!', 404) from error
-        raise PyPokedexHTTPError('An HTTP error occurred! '
-                                 '(Status code: {response.status_code})',
-                                 response.status_code) from error
+            raise PyPokedexHTTPError(
+                "The requested pokemon was not " "found!", 404
+            ) from error
+        raise PyPokedexHTTPError(
+            "An HTTP error occurred! " "(Status code: {response.status_code})",
+            response.status_code,
+        ) from error
 
     except requests.exceptions.RequestException as error:
-        raise PyPokedexError('An internal requests exception '
-                             'occurred!') from error
+        raise PyPokedexError("An internal requests exception " "occurred!") from error
 
     return Pokemon(response.json())
--- pypokedex/pokemon.py
+++ pypokedex/pokemon.py
@@ -30,51 +30,53 @@ class Pokemon:
         """Loads and stores required pokemon data"""
         # self._json_data = json_data # Store for repr
         try:
-            self.dex = json_data['id']
+            self.dex = json_data["id"]
 
-            for pokemon_info in ['name', 'height', 'weight']:
+            for pokemon_info in ["name", "height", "weight"]:
                 setattr(self, pokemon_info, json_data[pokemon_info])
 
             stat_dict = {}
 
-            for stat in json_data['stats']:
-                stat_value = stat['base_stat']
-                stat_name = stat['stat']['name']
+            for stat in json_data["stats"]:
+                stat_value = stat["base_stat"]
+                stat_name = stat["stat"]["name"]
 
-                if stat_name in ['hp', 'attack', 'defense', 'speed']:
+                if stat_name in ["hp", "attack", "defense", "speed"]:
                     stat_dict[stat_name] = stat_value
-                elif stat_name == 'special-attack':
-                    stat_dict['sp_atk'] = stat_value
-                elif stat_name == 'special-defense':  # pragma: no branch
-                    stat_dict['sp_def'] = stat_value
+                elif stat_name == "special-attack":
+                    stat_dict["sp_atk"] = stat_value
+                elif stat_name == "special-defense":  # pragma: no branch
+                    stat_dict["sp_def"] = stat_value
 
             self.base_stats = BaseStats(**stat_dict)
 
-            self.abilities = [Ability(ability['ability']['name'],
-                                      ability['is_hidden'])
-                              for ability in json_data['abilities']]
+            self.abilities = [
+                Ability(ability["ability"]["name"], ability["is_hidden"])
+                for ability in json_data["abilities"]
+            ]
 
-            self.types = [type_['type']['name']
-                          for type_ in json_data['types']]
+            self.types = [type_["type"]["name"] for type_ in json_data["types"]]
 
             self.moves: DefaultDict[str, List[Move]] = defaultdict(list)
 
-            for move in json_data['moves']:
-                move_name = move['move']['name']
+            for move in json_data["moves"]:
+                move_name = move["move"]["name"]
 
-                for game_details in move['version_group_details']:
-                    learn_level = game_details['level_learned_at']
-                    learn_method = game_details['move_learn_method']['name']
-                    game_name = game_details['version_group']['name']
+                for game_details in move["version_group_details"]:
+                    learn_level = game_details["level_learned_at"]
+                    learn_method = game_details["move_learn_method"]["name"]
+                    game_name = game_details["version_group"]["name"]
                     if learn_level == 0:  # Move not learned by level-up
                         learn_level = None
 
-                    self.moves[game_name].append(Move(move_name, learn_method,
-                                                      learn_level))
+                    self.moves[game_name].append(
+                        Move(move_name, learn_method, learn_level)
+                    )
 
         except KeyError as error:
-            raise PyPokedexError('A required piece of data was not found for'
-                                 'the current Pokemon!') from error
+            raise PyPokedexError(
+                "A required piece of data was not found for" "the current Pokemon!"
+            ) from error
 
     def exists_in(self, game: str) -> bool:
         """Checks whether the current Pokemon exists in the specified game."""
@@ -84,8 +86,9 @@ class Pokemon:
         """Checks whether the current Pokemon learn the specified move
         in the specified game."""
         if not self.exists_in(game):
-            raise PyPokedexError(f'{self.name} is not '  # type: ignore
-                                 f'obtainable in {game}!')
+            raise PyPokedexError(
+                f"{self.name} is not " f"obtainable in {game}!"  # type: ignore
+            )
 
         for move in self.moves[game]:
             if move.name == move_name:
@@ -97,7 +100,7 @@ class Pokemon:
 
     def __str__(self) -> str:
         """Returns a human-readable represenation of the current Pokemon."""
-        return f'Pokemon(dex={self.dex}, name=\'{self.name}\')'  # type: ignore
+        return f"Pokemon(dex={self.dex}, name='{self.name}')"  # type: ignore
 
     def __eq__(self, other) -> bool:
         return self.dex == other.dex

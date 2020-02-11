447989dfe9e42cea8d407e142b4c331f18629864
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-11 15:53:52 -0500

    Expand public api of package and declare all members

--- pypokedex/__init__.py
+++ pypokedex/__init__.py
@@ -1,3 +1,5 @@
 from pypokedex.api import get
+from pypokedex.pokemon import Pokemon, BaseStats, Ability, Sprites, Move
 
 __version__ = "1.3.1"
+__all__ = ["get", "Pokemon", "BaseStats", "Ability", "Sprites", "Move"]

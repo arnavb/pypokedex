5f1f736b92abc7711674279fd4576d170a92252f
Arnav Borborah <arnavborborah11@gmail.com>
2020-08-21 13:27:12 -0400

    v1.5.1

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ fi
 echo "Built pypokedex in dist folder"
 
 # Requires rename utility with perl replace support
-if ! rename 's/py3/py36/' dist/pypokedex-1.5.0-py3-none-any.whl; then
+if ! rename 's/py3/py36/' dist/pypokedex-1.5.1-py3-none-any.whl; then
   echo "Unable to rename built wheel. Aborting."
   exit 1
 fi

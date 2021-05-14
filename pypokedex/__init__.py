1060b09c5676dc87ebd1215b32a2aeef5edacee4
Arnav Borborah <arnavborborah11@gmail.com>
2021-05-14 14:47:52 -0400

    v1.6.0

--- pypokedex/__init__.py
+++ pypokedex/__init__.py
@@ -1,5 +1,5 @@
 from pypokedex.api import get
 from pypokedex.pokemon import Ability, BaseStats, Move, Pokemon, Sprites
 
-__version__ = "1.5.1"
+__version__ = "1.6.0"
 __all__ = ["get", "Pokemon", "BaseStats", "Ability", "Sprites", "Move"]
